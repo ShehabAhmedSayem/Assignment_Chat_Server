129f8c9d3f5503426fad92daacab8954a9c90c8b
Shehab Ahmed <sayem@Shehabs-MacBook-Pro.local>
2022-04-11 23:02:32 +0600

    Added url for ping

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from chat.views import new_message, reply_message
+from chat.views import pong, new_message, reply_message
 
 urlpatterns = [
+    path('ping/', pong, name="pong"),
     path('new-message/', new_message, name="new_message"),
     path('reply-message/', reply_message, name="reply_message"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -8,15 +8,21 @@ from chat.constants import RANDOM_MESSAGE_LIST
 
 
 @csrf_exempt
+def pong(request):
+    if request.method == 'GET':
+        return JsonResponse({'message': 'OK'}, status=200)
+
+
+@csrf_exempt
 def new_message(request):
     if request.method == 'POST':
         message = request.POST.get('message', None)
-        from_client_address = request.POST.get('from_client_address', None)
+        sender_client_address = request.POST.get('sender_client_address', None)
         for m in RANDOM_MESSAGE_LIST:
             if m["question"] == message:
                 reply = m["reply"]
                 break
-        print(f"\nNew message : \nFrom : {from_client_address}\nMessage: {message}\n")
+        print(f"\nNew message : \nFrom : {sender_client_address}\nMessage: {message}\n")
         if not settings.AUTO_REPLY:
             try:
                 reply = inputimeout("Please send a reply: ", timeout=9)
@@ -30,6 +36,6 @@ def reply_message(request):
     if request.method == 'POST':
         message = request.POST.get('message', None)
         reply = request.POST.get('reply', None)
-        from_client_address = request.POST.get('from_client_address', None)
-        print(f"\nReply : \nFrom: {from_client_address}\nMessage : {message}\nReply : {reply}\n")
-        return JsonResponse({'message': "OK"}, status=200)
+        sender_client_address = request.POST.get('sender_client_address', None)
+        print(f"\nReply : \nFrom: {sender_client_address}\nMessage : {message}\nReply : {reply}\n")
+        return JsonResponse({'message': 'OK'}, status=200)
